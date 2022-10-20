b7f138892e601162c0901993f39eeecc00bd468d
Long Vo <vodoanhoanglong10a9@gmail.com>
2022-10-20 10:05:49 +0700

    Allow cors policy

---
+++ src/main/java/dev/ecommerce/Configuration.java
@@ -0,0 +1,21 @@
+package dev.ecommerce;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/graphql/**", config);
+        return new CorsFilter(source);
+    }
+}
