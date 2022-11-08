bed1e3b06e42f6704b9d8654294f47e8b4186f39
Nguyễn Thanh Hòa <nthoa2.lhp.nh@gmail.com>
2022-11-08 18:56:47 +0700

    Handled query Product with pagination

--- src/main/java/dev/ecommerce/repositories/ProductsRepository.java
+++ src/main/java/dev/ecommerce/repositories/ProductsRepository.java
@@ -1,10 +1,15 @@
 package dev.ecommerce.repositories;
 
 import dev.ecommerce.models.Products;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Products, String>, JpaSpecificationExecutor<Products> {
+    @NotNull Page<Products> findAll(@NotNull Pageable pageable);
 }
---
+++ src/main/java/dev/ecommerce/resolvers/ProductResolvers.java
@@ -0,0 +1,48 @@
+package dev.ecommerce.resolvers;
+
+import dev.ecommerce.models.Products;
+import dev.ecommerce.repositories.ProductsRepository;
+import dev.ecommerce.DTO.PaginationData;
+import dev.ecommerce.DTO.PaginationInput;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class ProductResolvers {
+    @Autowired
+    ProductsRepository productsRepository;
+
+    @QueryMapping
+    public Map<String, Object> getProducts(@Argument PaginationInput paginate){
+        Map<String, Object> response = new HashMap<>();
+        int pageNumber, pageSize;
+        if(paginate == null) {
+            pageNumber = 0;
+            pageSize = 5;
+        }
+        else {
+            pageNumber = paginate.getPage();
+            pageSize = paginate.getSize();
+        }
+        try{
+            Pageable paging = PageRequest.of(pageNumber, pageSize);
+            Page<Products> pageProds  = productsRepository.findAll(paging);
+            List<Products> productsList = pageProds.getContent();
+            PaginationData pagination = new PaginationData(pageProds.getTotalElements(), pageProds.getNumber(), pageProds.getTotalPages(), pageProds.getSize());
+            response.put("data", productsList);
+            response.put("pagination", pagination);
+        }catch(Error error){
+            response.put("Error", error.getMessage());
+        }
+        return  response;
+    }
+}
