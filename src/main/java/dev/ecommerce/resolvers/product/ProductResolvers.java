19fcbbea357f35747a831aad18ffc15f53fcf3f7
Nguyễn Thanh Hòa <nthoa2.lhp.nh@gmail.com>
2022-11-11 10:52:01 +0700

    replace http req to graphQL Cỏntext

--- src/main/java/dev/ecommerce/resolvers/product/ProductResolvers.java
+++ src/main/java/dev/ecommerce/resolvers/product/ProductResolvers.java
@@ -1,5 +1,6 @@
 package dev.ecommerce.resolvers.product;
 
+import dev.ecommerce.models.Users;
 import dev.ecommerce.resolvers.product.schema.PaginationInput;
 import dev.ecommerce.resolvers.product.schema.ProductReqBody;
 import dev.ecommerce.models.ProductImages;
@@ -11,6 +12,8 @@ import dev.ecommerce.resolvers.product.schema.PaginationData;
 import dev.ecommerce.repositories.ShopsRepository;
 import dev.ecommerce.shared.auth.JwtTokenProvider;
 import dev.ecommerce.shared.resources.Errors;
+import dev.ecommerce.shared.resources.Headers;
+import graphql.GraphQLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -25,10 +28,6 @@ import java.util.*;
 
 @Controller
 public class ProductResolvers {
-    @Autowired
-    HttpServletRequest request;
-    @Autowired
-    JwtTokenProvider jwtTokenProvider;
     @Autowired
     ProductsRepository productsRepository;
     @Autowired
@@ -61,12 +60,11 @@ public class ProductResolvers {
     }
 
     @MutationMapping
-    public Map<String, Object> createNewProduct(@Argument ProductReqBody product) {
+    public Map<String, Object> createNewProduct(GraphQLContext graphQLContext, @Argument ProductReqBody product) {
         try {
             Map<String, Object> res = new HashMap<>();
-            String token = request.getHeader("Authorization").split(" ")[1];
-            String userID = jwtTokenProvider.getUserIdFromJWT(token);
-            String shopID = shopsRepository.getShopsByCreatedBy(userID).getId();
+            Users currentUser = graphQLContext.get(Headers.CurrentUser.getValue());
+            String shopID = shopsRepository.getShopsByCreatedBy(currentUser.getId()).getId();
             Products duplicate = productsRepository.findProductsByName(product.getName());
             if (duplicate != null) {
                 throw new Error(Errors.ProductAlreadyExist.getValue());
@@ -75,13 +73,13 @@ public class ProductResolvers {
             String name = product.getName();
             String desc = product.getDescription();
             Float price = product.getPrice();
-            Products newProduct = productsRepository.save(new Products(String.valueOf(id), name, desc, price, 0L, userID, shopID));
+            Products newProduct = productsRepository.save(new Products(String.valueOf(id), name, desc, price, 0L, currentUser.getId(), shopID));
             List<String> imagesUrls = product.getImages();
             if (imagesUrls != null && !imagesUrls.isEmpty()) {
                 List<ProductImages> images = new ArrayList<>();
                 imagesUrls.forEach(url -> {
                     UUID imageID = UUID.randomUUID();
-                    ProductImages newImage = productImagesRepository.save(new ProductImages(String.valueOf(imageID), url, String.valueOf(id), String.valueOf(userID)));
+                    ProductImages newImage = productImagesRepository.save(new ProductImages(String.valueOf(imageID), url, String.valueOf(id), currentUser.getId()));
                     images.add(newImage);
                 });
                 newProduct.setImages(images);
