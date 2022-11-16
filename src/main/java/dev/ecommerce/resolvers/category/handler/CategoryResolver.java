0e2afe632ce268e9a5b1333e050c6fbd5afe2cbb
Oharu123 <beob345@gmail.com>
2022-11-16 21:21:59 +0700

    feature/categories

--- src/main/java/dev/ecommerce/resolvers/category/handler/CategoryResolver.java
+++ src/main/java/dev/ecommerce/resolvers/category/handler/CategoryResolver.java
@@ -34,17 +34,17 @@ public class CategoryResolver {
 
     @MutationMapping
     @Transactional
-    public Map<String, Object> createCategory(@Argument @Valid FormCreateCategoryInput categoryform){
+    public Map<String, Object> createCategory(@Argument @Valid FormCreateCategoryInput form){
         try {
-            HashMap<String, Object> cate = new HashMap<>();
-            if (cate != null) {
+            HashMap<String, Object> category = new HashMap<>();
+            if (category != null) {
                 throw new Error(Errors.CategoriesAlreadyExist.getValue());
             }
-            Categories addCategory = new Categories(categoryform.getAlias(), categoryform.getName()
-                    , categoryform.getDescription());
+            Categories addCategory = new Categories(form.getAlias(), form.getName()
+                    , form.getDescription());
             entityManager.persist(addCategory);
-            cate.put("isSuccess", true);
-            return cate;
+            category.put("isSuccess", true);
+            return category;
         }catch (Error err){
             throw new CustomMessageError(err.getMessage());
         }
@@ -54,27 +54,25 @@ public class CategoryResolver {
     public List<Categories> getCategory(){
         try{
             return categoriesRepository.findAll().stream().toList();
-        }catch(Error error){
-            throw new CustomMessageError(error.getMessage());
+        }catch(Error err){
+            throw new CustomMessageError(err.getMessage());
         }
     }
 
     @MutationMapping
     @Transactional
-    public Map<String, String> updateCategory(@Argument FormCreateCategoryInput categoryform){
-        HashMap<String, String> cate = new HashMap<>();
+    public Map<String, String> updateCategory(@Argument FormCreateCategoryInput form){
+        HashMap<String, String> category = new HashMap<>();
 
         try {
-            Categories categoriesFromDB = categoriesRepository.findByAlias(categoryform.getAlias());;
-            categoriesFromDB.setName(categoryform.getName());
-            categoriesFromDB.setDescription(categoryform.getDescription());
-            cate.put("name", categoriesFromDB.getName());
-            cate.put("description", categoriesFromDB.getDescription());
+            Categories categoriesFromDB = categoriesRepository.findByAlias(form.getAlias());;
+            categoriesFromDB.setName(form.getName());
+            categoriesFromDB.setDescription(form.getDescription());
+            category.put("name", categoriesFromDB.getName());
+            category.put("description", categoriesFromDB.getDescription());
+            return category;
         }catch (Error err){
-            cate.put("isSuccess", "false");
-            cate.put("token", null);
-            cate.put("error", err.getMessage());
+            throw new CustomMessageError(err.getMessage());
         }
-        return cate;
     }
 }
